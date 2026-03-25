437ac1a42ad3ad8d454770190f748d8d00fa5461
Antoine <antoineho6@gmail.com>
2026-03-24 20:43:46 -0400

    Solved problem 18 AND problem 67

--- src/problem_18.rs
+++ src/problem_18.rs
@@ -1,12 +1,10 @@
-use std::fs;
+use std::{cmp::max, fs};
 
 pub fn problem_18() {
     let content: String = fs::read_to_string("src/problem_18_data.txt").expect("");
 
-    let rows_str: Vec<&str> = content.split("<br>").collect();
+    let rows_str: Vec<&str> = content.lines().collect();
 
-    let starting_row = rows_str.len() - 2;
-    
     let mut rows: Vec<Vec<i32>> = rows_str
                                         .iter()
                                         .map(
@@ -14,14 +12,18 @@ pub fn problem_18() {
                                                 |s| s.parse::<i32>().unwrap()
                                             ).collect()
                                         ).collect();
-    
-    if let Some(row) = rows.get(starting_row) {
-        for num in row {
-            println!("Value: {}", num);
-        }
-    }
-}
 
-fn calculate_max_sum(row_idx: i32) {
-    
+    for i in (0..=rows.len()-2).rev() {
+        for j in 0..rows[i].len() {
+            let left_child = rows[i][j] + rows[i+1][j];
+            let right_child = rows[i][j] + rows[i+1][j+1];
+
+            rows[i][j] = max(left_child, right_child); 
+
+            print!("{} ", rows[i][j]);
+        }
+        println!(); 
+    }
+
+    println!("{}", rows[0][0]);
 }
